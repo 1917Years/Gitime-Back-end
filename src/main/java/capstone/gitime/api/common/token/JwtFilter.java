4bd28b0349d00cf517432a9e53a6dc3b4d53b9d9
hoyaspark <hoyaspark@gmail.com>
2022-04-28 12:12:04 +0900

    feat : CI / CD 적용 테스트

--- src/main/java/capstone/gitime/api/common/token/JwtFilter.java
+++ src/main/java/capstone/gitime/api/common/token/JwtFilter.java
@@ -26,6 +26,7 @@ public class JwtFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         String token = getToken(request);
+        System.out.println("I NEED COMMIT");
 
         if (StringUtils.hasText(token) && tokenProvider.validateToken(token)) {
             Authentication authentication = tokenProvider.getAuthentication(token);
